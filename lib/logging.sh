686177a32ae91951fc56949ed5bb3880d83e4554
Hamish Morgan <hamish.morgan@gmail.com>
2017-03-03 13:05:43 +0000

    Split logging stuff out into it's own file

---
+++ lib/logging.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+### Logging
+
+OFF=-1
+ERROR=0
+WARN=1
+INFO=2
+DEBUG=3
+TRACE=4
+
+verbosity=${DEBUG}
+
+function error {
+    if [ ${verbosity} -ge ${TRACE} ]; then
+        echo -e "\e[31m$@\e[0m"
+    fi
+}
+
+function warn {
+    if [ ${verbosity} -ge ${WARN} ]; then
+        echo -e "\e[33m$@\e[0m"
+    fi
+}
+
+function info {
+    if [ ${verbosity} -ge ${INFO} ]; then
+        echo -e "\e[32m$@\e[0m"
+    fi
+}
+
+function debug {
+    if [ ${verbosity} -ge ${DEBUG} ]; then
+        echo -e "\e[2m$@\e[0m"
+    fi
+}
+
+function trace {
+    if [ ${verbosity} -ge ${TRACE} ]; then
+        printf '  %.0s' $(seq 1 ${#FUNCNAME[@]})
+        echo -e "\e[35m${FUNCNAME[1]}($@)\e[0m"
+    fi
+}
+
+
+## Exit status
+
+function fail {
+    error $@
+    exit 1
+}
+
+function pass {
+    warn $@
+}
+
+function success {
+    info $@
+}
--- lib/shared.sh
+++ lib/shared.sh
@@ -14,63 +14,8 @@ DOTFILES_BASE_DIR=$(dirname $(realpath -sm $0/..))
 TRUE=0
 FALSE=1
 
-### Logging
 
-OFF=-1
-ERROR=0
-WARN=1
-INFO=2
-DEBUG=3
-TRACE=4
-
-verbosity=${DEBUG}
-
-function error {
-    if [ ${verbosity} -ge ${TRACE} ]; then
-        echo -e "\e[31m$@\e[0m"
-    fi
-}
-
-function warn {
-    if [ ${verbosity} -ge ${WARN} ]; then
-        echo -e "\e[33m$@\e[0m"
-    fi
-}
-
-function info {
-    if [ ${verbosity} -ge ${INFO} ]; then
-        echo -e "\e[32m$@\e[0m"
-    fi
-}
-
-function debug {
-    if [ ${verbosity} -ge ${DEBUG} ]; then
-        echo -e "\e[2m$@\e[0m"
-    fi
-}
-
-function trace {
-    if [ ${verbosity} -ge ${TRACE} ]; then
-        printf '  %.0s' $(seq 1 ${#FUNCNAME[@]})
-        echo -e "\e[35m${FUNCNAME[1]}($@)\e[0m"
-    fi
-}
-
-
-## Exit status
-
-function fail {
-    error $@
-    exit 1
-}
-
-function pass {
-    warn $@
-}
-
-function success {
-    info $@
-}
+source ${DOTFILES_BASE_DIR}/lib/logging.sh
 
 ## Git
 
