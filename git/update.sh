6c301a4f172d1c5dd4299ace253c8a60c91f8afe
Hamish Morgan <hamish.morgan@gmail.com>
2017-03-03 13:02:43 +0000

    NT Moved shared stuff /lib

--- git/update.sh
+++ git/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DOTFILES_BASE_DIR=$(dirname $(realpath -sm $0/..))
-source ${DOTFILES_BASE_DIR}/bin/.shared.sh
+source ${DOTFILES_BASE_DIR}/lib/shared.sh
 
 ### Global gitignore config
 
--- vim/update.sh
+++ vim/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DOTFILES_BASE_DIR=$(dirname $(realpath -sm $0/..))
-source ${DOTFILES_BASE_DIR}/bin/.shared.sh
+source ${DOTFILES_BASE_DIR}/lib/shared.sh
 
 cd ${DOTFILES_BASE_DIR}/vim/.spf13-vim-3/
 git pull
