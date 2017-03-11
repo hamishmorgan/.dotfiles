8ff2b21f0253612928e091e0010ba1b5d3e1fa40
Hamish Morgan <hamish.morgan@gmail.com>
2017-03-11 11:33:31 +0000

    Added update scripts of zsh

---
+++ bin/update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+DOTFILES_BASE_DIR=$(dirname $(realpath -sm $0/..))
+
+. $DOTFILES_BASE_DIR/modules/zsh/update.sh
+
---
+++ modules/zsh/update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+zsh -f $ZSH/tools/upgrade.sh
+
+
