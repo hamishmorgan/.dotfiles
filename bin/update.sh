597478d1fda55bff1fa4475cd5ff3a2dfdc43d96
Hamish Morgan <hamish.morgan@gmail.com>
2018-10-28 07:41:27 +0000

    Moved update script to root

--- bin/update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-DOTFILES_BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-DOTFILES_BASE_DIR=${DOTFILES_BIN_DIR}/..
-
-. $DOTFILES_BASE_DIR/modules/zsh/update.sh
-
---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eu
+
+DOTFILES_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+
+
+
+git submodule update --remote dotbot
+
+
+
+. $DOTFILES_BASE_DIR/modules/zsh/update.sh
+
