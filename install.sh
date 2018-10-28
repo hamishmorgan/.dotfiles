73e45485ac0408b7543b67f7e446b308aef157dd
Hamish Morgan <hamish.morgan@gmail.com>
2018-10-28 07:29:32 +0000

    Updated dotbot install file with upstream fix

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ DOTBOT_BIN="bin/dotbot"
 DOTFILES_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd "${DOTFILES_BASE_DIR}"
+
+git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
 "${DOTFILES_BASE_DIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${DOTFILES_BASE_DIR}" -c "${CONFIG}" "${@}"
