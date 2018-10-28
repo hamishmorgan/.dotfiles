ac47b584a58cbb5597b693c2626d5be5f7578b20
Hamish Morgan <hamish.morgan@gmail.com>
2018-10-28 09:28:08 +0000

    Deleted the vim update script because it's broken

--- modules/vim/update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-DOTFILES_VIM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-DOTFILES_BASE_DIR=${DOTFILES_VIM_DIR}/../..
-
-cd ${DOTFILES_BASE_DIR}/vim/.spf13-vim-3/
-git pull
-vim +BundleInstall! +BundleClean +q
