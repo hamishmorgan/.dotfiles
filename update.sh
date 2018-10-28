5cfb13c0ae69dfa7dc743b457f6516f84dcd4a04
Hamish Morgan <hamish.morgan@gmail.com>
2018-10-28 08:13:56 +0000

    Removed stop on error from global update script

--- update.sh
+++ update.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-set -eu
+set -u
 
 DOTFILES_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 
-
-
 (git submodule update --remote dotbot)
 
-
 (. $DOTFILES_BASE_DIR/modules/zsh/update.sh)
-
 
 (. $DOTFILES_BASE_DIR/modules/git/update.sh)
 
