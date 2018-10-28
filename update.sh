60a0615d139b4cbeaab409315b16c48ace279c48
Hamish Morgan <hamish.morgan@gmail.com>
2018-10-28 08:03:39 +0000

    Added git to global update script

--- update.sh
+++ update.sh
@@ -9,6 +9,8 @@ DOTFILES_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 git submodule update --remote dotbot
 
 
-
 . $DOTFILES_BASE_DIR/modules/zsh/update.sh
+
+
+. $DOTFILES_BASE_DIR/modules/git/update.sh
 
