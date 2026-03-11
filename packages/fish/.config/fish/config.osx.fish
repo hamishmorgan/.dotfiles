a966758f1f56324919a3f562f3f1d84b5be448d4
Hamish Morgan <hamish.morgan@shopify.com>
2026-03-11 10:42:09 +0000

    fish: move brew shellenv to conf.d for correct PATH order
    brew shellenv was in config.osx.fish (sourced from config.fish, which
    runs AFTER conf.d/). tec init in conf.d/shopify.fish ran first, then
    brew prepended /opt/homebrew/bin on top, shadowing tec binaries.
    
    Moving to conf.d/brew.fish (b < s alphabetically) ensures brew runs
    before tec init in conf.d/shopify.fish, so tec prepends last and
    takes precedence. Fixes 'PATH order issue detected' warning from
    dev up.

--- packages/fish/.config/fish/config.osx.fish
+++ packages/fish/.config/fish/config.osx.fish
@@ -1,15 +1,6 @@
 # macOS-specific fish configuration
+# Note: Homebrew setup is in conf.d/brew.fish (needs to run before tec init)
 switch $FISH_HOST_OS
     case darwin
-        # macOS-specific settings
-
-        # Homebrew setup
-        if test -x /opt/homebrew/bin/brew
-            eval (/opt/homebrew/bin/brew shellenv)
-        else if test -x /usr/local/bin/brew
-            eval (/usr/local/bin/brew shellenv)
-        end
-
-        # Add any other macOS-specific environment variables or aliases here
+        # Add macOS-specific environment variables or aliases here
 end
-
