7d034fc14701b7dc4fe404da0ecebbb31191a188
Hamish Morgan <hamish.morgan@gmail.com>
2026-04-05 19:49:45 +0100

    Fix niri activation ordering and CI step name
    - niri: add lib.hm.dag.entryAfter ["writeBoundary"] so the config
      symlink runs after home directory setup, not in undefined order
    - CI: correct step name from nixpkgs-fmt to nixfmt

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -9,7 +9,7 @@ lib.mkIf pkgs.stdenv.isLinux {
   # Create a direct symlink (not through nix store) so niri's
   # inotify-based live-reload works. Two-level symlinks via
   # mkOutOfStoreSymlink break inotify.
-  home.activation.niriConfig = ''
+  home.activation.niriConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     ln -sfn "${dotfilesPath}/home/niri/config.kdl" "$HOME/.config/niri/config.kdl"
   '';
 
